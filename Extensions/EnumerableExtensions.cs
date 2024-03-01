a9a5985dc28b94965296770efd4e41b3cf6e048b
Nikita Serebriakov <whaledevelop@gmail.com>
2024-03-01 09:07:01 +0400

    Remove local extensions

--- Extensions/EnumerableExtensions.cs
+++ Extensions/EnumerableExtensions.cs
@@ -7,37 +7,6 @@ namespace Whaledevelop.Extensions
 {
     public static class EnumerableExtensions
     {
-        public static int IndexOf<T>(this IEnumerable<T> self, Predicate<T> predicate)
-        {
-            var index = 0;
-
-            foreach (var element in self)
-            {
-                if (predicate.Invoke(element))
-                {
-                    return index;
-                }
-                index++;
-            }
-            return -1;
-        }
-
-        public static void ForEach<T>(this IEnumerable<T> self, Action<T> action)
-        {
-            foreach (var element in self)
-            {
-                action.Invoke(element);
-            }
-        }
-
-        public static void ToList<T>(this IEnumerable<T> self, ICollection<T> collection)
-        {
-            foreach (var element in self)
-            {
-                collection.Add(element);
-            }
-        }
-
         public static IEnumerable<T> Cache<T>(this IEnumerable<T> self)
         {
             using (ListScope<T>.Create(out var items))
@@ -49,18 +18,6 @@ namespace Whaledevelop.Extensions
                     yield return item;
                 }
             }
-        }
-
-        public static bool TryGetFirstElement<T>(this IEnumerable<T> self, out T value)
-        {
-            foreach (var item in self)
-            {
-                value = item;
-                return true;
-            }
-
-            value = default;
-            return false;
         }
 
         public static bool TryGetFirst<T>(this IEnumerable<T> self, Predicate<T> predicate, out T value)
@@ -80,38 +37,6 @@ namespace Whaledevelop.Extensions
             return false;
         }
 
-        public static bool TryGetLastElement<T>(this IEnumerable<T> self, out T value)
-        {
-            var found = false;
-            value = default;
-            foreach (var item in self)
-            {
-                found = true;
-                value = item;
-            }
-
-            return found;
-        }
-
-        public static bool TryGetLast<T>(this IEnumerable<T> self, Predicate<T> predicate, out T value)
-        {
-            var found = false;
-            value = default;
-
-            foreach (var item in self)
-            {
-                if (!predicate(item))
-                {
-                    continue;
-                }
-
-                value = item;
-                found = true;
-            }
-
-            return found;
-        }
-
         public static List<T> ToList<T>(this IEnumerable<T> self, List<T> list)
         {
             return self.ToList(list, false);
@@ -127,20 +52,6 @@ namespace Whaledevelop.Extensions
             list.AddRange(self);
 
             return list;
-        }
-
-        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
-        {
-            var elements = source.ToArray();
-            for (var i = elements.Length - 1; i > 0; i--)
-            {
-                var swapIndex = UnityEngine.Random.Range(0, i + 1);
-                (elements[i], elements[swapIndex]) = (elements[swapIndex], elements[i]);
-            }
-            foreach (var element in elements)
-            {
-                yield return element;
-            }
         }
     }
 }
