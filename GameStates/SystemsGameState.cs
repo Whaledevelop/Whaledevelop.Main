3e7c5542166e46cd5154b648ba830b36e2f54a5a
Nikita Serebriakov <whaledevelop@gmail.com>
2025-05-11 17:26:14 +0300

    SystemsGameState

---
+++ GameStates/SystemsGameState.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Whaledevelop.GameStates
+{
+    [CreateAssetMenu(menuName = "Whaledevelop/States/SystemsGameState", fileName = "NewSystemsGameState")]
+    public class SystemsGameState : ScriptableGameState
+    {
+        [SerializeReference] 
+        private GameSystemScriptable[] _systems;
+
+        [Inject] 
+        private IGameSystemsService _gameSystemsService;
+        
+        protected override UniTask OnInitializeAsync(CancellationToken cancellationToken)
+        {
+            return _gameSystemsService.UpdateSystemsAsync(_systems, cancellationToken);
+        }
+    }
+}
